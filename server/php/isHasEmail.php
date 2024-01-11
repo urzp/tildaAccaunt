311afa50a2a3c48ee7942cde067002c97c1e2726
urzp <ermak80_pass@mail.ru>
2024-01-11 22:50:26 +0300

    check email

---
+++ server/php/isHasEmail.php
@@ -0,0 +1,23 @@
+<?php
+
+//https://ermakpass.ru/media_node/php/islogged.php
+
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+
+$post = file_get_contents("php://input");
+$post = json_decode($post);
+
+
+$email = $post-> email;
+
+include 'db_mysql.php';
+include 'checkEmail.php';
+
+$result = checkEmail($email, $mysql);
+
+
+$mysql->close();
+echo json_encode($result);
+
+?>
