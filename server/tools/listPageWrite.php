bf9aa344b1237131b855d4b7913089980588eeb8
urzp <ermak80_pass@mail.ru>
2024-04-26 15:41:25 +0300

    read pages

--- server/tools/listPageWrite.php
+++ server/tools/listPageWrite.php
@@ -4,31 +4,63 @@ include '../config.php';
 header('Access-Control-Allow-Origin: *');
 header("Content-Type: application/json");
 
-function notePageTable($name, $title){
+function notePageTable($id, $folder_id , $url, $title, $img){
     global $mysql;
-
     $sql = "INSERT INTO `pages` 
-    (`name`,`title`) 
+    (`id`, `folder_id`, `url`, `title`, `img`) 
     VALUES
-    ('$name','$title')";
+    ('$id', '$folder_id' ,'$url','$title', '$img')";
     $mysql -> query($sql);
 }
 
-$post = file_get_contents("php://input");
-$post = json_decode($post);
-$data = $post;
-
-foreach($data as $item){
-    $name = $item -> link;
-    $title = $item -> title;
-    notePageTable($name, $title);
+function noteFolderTable($id, $name){
+    global $mysql;
+    $sql = "INSERT INTO `folders` 
+    (`id`, `name`) 
+    VALUES
+    ('$id', '$name')";
+    $mysql -> query($sql);
 }
 
-$result = (object) [
-    'success' => true,
-    'data' => $data[1] -> link,
-];
-echo json_encode($result);
+function send_response($success, $msg = ''){
+    $result = (object) [
+        'success' => $success,
+        'msg' => $msg,
+    ];    
+    echo json_encode($result);
+    exit();
+}
+
+//-------------------- BEGIN ---------------------
+
+$post = file_get_contents("php://input");
+$post = json_decode($post);
+$pages = $post -> pages;
+$folders = $post -> folders;
+
+if(!isset($pages)){ send_response(false, 'No one page');}
+$sql = "DELETE FROM `pages`";
+$mysql -> query($sql);
+
+foreach($pages as $item){
+    $id = $item -> id;
+    $folder_id = $item -> folderid;
+    $url = $item -> url;
+    $title = $item -> title;
+    $img = $item -> img;
+    notePageTable($id, $folder_id, $url, $title, $img);
+}
+
+$sql = "DELETE FROM `folders`";
+$mysql -> query($sql);
+
+foreach($folders as $item){
+    $id = $item -> id;
+    $name = $item -> title;
+    noteFolderTable($id, $name);
+}
+
+send_response(true);
 
 ?>
 
--- tilda/tools/parse_list _pages.js
+++ tilda/tools/parse_list _pages.js
@@ -1,15 +1,6 @@
-let data = []
-
-$('.td-project-pages-list .td-page__table').each(function(){
-    let link =  $(this).find('.td-page__td-url a').text().slice(1)
-    let title = $(this).find('.td-page__td-title .td-page__td-title-span').text()
-    data.push({
-        link,
-        title
-    })
-})
-
-
+const URL_UPDATE_PAGES = 'https://smmbackmy.ru/php/tools/listPageWrite.php'
+const AJAX_OPTIONS = { type: 'POST', headers: { 'Content-Type': 'application/json;charset=utf-8' }, }
+let data = { folders, pages }
 
 async function ajaxReq(url, options, content){
     let response = await fetch(url,{
@@ -20,6 +11,4 @@ async function ajaxReq(url, options, content){
     return await response.json();
 }
 
-const URL_GET_BALANS = 'https://smmbackmy.ru/php/tools/listPageWrite.php'
-const AJAX_OPTIONS = { type: 'POST', headers: { 'Content-Type': 'application/json;charset=utf-8' }, }
-let result = await ajaxReq(URL_GET_BALANS, AJAX_OPTIONS, data)
+let result = await ajaxReq(URL_UPDATE_PAGES, AJAX_OPTIONS, data)
