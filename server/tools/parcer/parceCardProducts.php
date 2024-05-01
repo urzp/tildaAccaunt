e0a47646d19af87ea8180a4d40a879fa0ad7eafa
urzp <ermak80_pass@mail.ru>
2024-05-01 22:28:21 +0300

    function parce cards

---
+++ server/tools/parcer/parceCardProducts.php
@@ -0,0 +1,80 @@
+<?php
+include 'config.php';
+include 'parserFunction.php';
+
+function noteCauntCatdsPage($id_page, $countCatds){
+    global $mysql;
+    $sql = "UPDATE`pages` SET `cards`='$countCatds' WHERE `id` = '$id_page'";
+    $mysql -> query($sql);    
+}
+
+function noteCardProduct($id_page, $url, $card, $i_card){
+    global $mysql;
+    $sql="SELECT `id` FROM `cardsProduct` WHERE `id_page`='$id_page' AND `number_in_page`='$i_card'";
+    $result = $mysql -> query($sql);
+    $result =  $result -> fetch_assoc();
+
+    if(isset($result)){
+        //update
+    }else{
+        $id_provider = $card['id_provider']; 
+        $id_servis = $card['id_servis'];
+        $img = $card['icon'];
+        $title = $card['title'];
+        $description = $card['description'];
+        $price = $card['price'];
+        $currency = $card['currency'];
+        $sql = "INSERT INTO `cardsProduct` 
+        (`id_page`,`number_in_page` , `id_provider`, `id_servis`, `url`, `img`, `title`, `description`, `price`, `currency`) 
+        VALUES
+        ('$id_page', '$i_card', '$id_provider', '$id_servis', '$url', '$img', '$title', '$description', '$price', '$currency' )";
+        $mysql -> query($sql);
+        echo '<br/>';
+        echo $sql;
+        echo '<br/>';
+    }
+}
+
+$sql="SELECT `value` FROM `config` WHERE `name`='mainLinkSite'";
+$mainURL = $mysql -> query($sql);
+$mainURL = $mainURL -> fetch_assoc();
+$mainURL = $mainURL['value'].'/';
+
+$sql="SELECT `id`, `url` FROM `pages`";
+$pages = $mysql -> query($sql);
+
+$i=0;
+foreach($pages as $item){
+    //if($i>=11 AND $i<12){
+        echo $i.'<br/>';
+        //echo $item['url'].'<br/>';
+       $result =  pesePage($mainURL, $item['url']);
+       noteCauntCatdsPage( $item['id'], count($result));
+       $id_page = $item['id'];
+       $url = $item['url'];
+        if(count($result)>0){
+            $i_card = 0;
+            foreach($result as $item){
+                $i_card++;
+                noteCardProduct($id_page, $url, $item, $i_card);
+                echo '<br/>';
+                echo $item['icon'];
+                echo '<br/>';
+                echo $item['title'];
+                echo '<br/>';
+                echo $item['price'];
+                echo '<br/>';
+                echo $item['id_provider'];
+                echo '<br/>';
+                echo '<br/>';
+            }
+        }else{
+            echo '<br/>';
+            echo 'no cards';
+            echo '<br/>';
+        }
+    //}
+    $i++;
+};
+
+?>
---
+++ server/tools/parcer/parserFunction.php
@@ -0,0 +1,117 @@
+<?php
+require __DIR__.'/phpQuery/phpQuery/phpQuery.php';
+ 
+function selectCurrency($text){
+    $currency = [];
+    $currency[0] = array(
+        'much_symb'=> '₽',
+        'result_symb'=> 'руб'
+    );
+
+    foreach($currency as $item){
+        $much_symb = $item['much_symb'];
+        $reg = "[$much_symb]";
+        if(preg_match($reg, $text, $matches)){
+            $res = $item['result_symb'];
+            return $res;
+        }
+    }
+
+    return '';
+}
+
+function pesePage($homeUrl, $page_url){
+    //sleep(1);
+   
+    $headers = array(
+        'cache-control: max-age=0',
+        'upgrade-insecure-requests: 1',
+        'user-agent: Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36',
+        'sec-fetch-user: ?1',
+        'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/web,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'x-compress: null',
+        'sec-fetch-site: none',
+        'sec-fetch-mode: navigate',
+        'accept-encoding: derflate, br',
+        'accept-language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'
+    );
+
+    $url = $homeUrl.$page_url;
+    
+    echo $url.'<br/>';
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_COOKIEFILE, __DIR__ . '/cookie.txt');
+    curl_setopt($ch, CURLOPT_COOKIEJAR, __DIR__ . '/cookie.txt');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 400);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    $html = curl_exec($ch);
+    curl_close($ch);
+
+    
+    echo 'ready';
+    $pq = phpQuery::newDocument($html);
+    
+    
+    
+    if(!isset($pq->find('.t1070__col')[0])){ 
+        return []; 
+        
+    };
+
+    $sctipt = $pq->find('.t123 script')[0]->text();   
+    $f_begin   = 'let array = {';
+    $f_end   = 'k = 1';
+    $pos_beg = strpos($sctipt, $f_begin)+14;
+    $pos_end = strpos($sctipt, $f_end) - $pos_beg - 2;
+    $str =  substr($sctipt, $pos_beg, $pos_end);
+    $str = str_replace('}','',$str);
+    $str = preg_replace('/(\ \/\*.*\*\/)/s', '',  $str);
+
+    $arr = explode("],", $str);
+    $arr_servis = [];
+
+    $i = 0;
+    foreach($arr as $item){
+        $item = explode('" : ', $item)[1];
+        $item = str_replace('[','',$item);
+        $item = str_replace(']','',$item);
+        $item = explode(', ', $item);
+        if($item[0]!= '1'){
+            $arr_servis[$i] = $item;
+            $i++;
+        }  
+    }
+
+    $result  = [];
+    $i=0;
+    foreach ($pq->find('.t1070__col') as $item){
+        $item = pq($item);
+        $icon = $item->find('.t1070__img')->attr('src');
+        $title = $item->find('.t-card__title div')->text();
+        $description = $item->find('.t-card__descr')->text();
+        $price_title = $item->find('.t1070__price')->text();
+        $price = preg_replace('/[^0-9,]+/', '', $price_title);
+        $price =  str_replace(',', '.', $price);
+        $currency = selectCurrency($price_title);
+        $result[$i] = array(
+            'icon' => $icon,
+            'title' => $title,
+            'description' => $description,
+            'price' => $price,
+            'id_provider' => $arr_servis[$i][1],
+            'id_servis' => $arr_servis[$i][0],
+            'currency' => $currency,
+        );
+        $i++;
+    }
+
+
+
+    return $result;
+}
+
+?>
