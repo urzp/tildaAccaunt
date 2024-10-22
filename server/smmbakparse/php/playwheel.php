f056eb91e011d8dfb8367fbf45907c15c4971a8b
urzp <ermak80_pass@mail.ru>
2024-10-22 09:08:53 +0300

    save

---
+++ server/smmbakparse/php/playwheel.php
@@ -0,0 +1,30 @@
+<?php
+
+include 'config_timer.php';
+include 'modules/crud.php';
+include 'modules/functions.php';
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+
+$post = file_get_contents("php://input");
+$post = json_decode($post);
+
+$sessionToken = $post -> sessionToken;
+
+$data['wheel'] = rand(0,5);
+if($data['wheel']==0){$data['wheel'] = rand(0,5);}
+
+if($data['wheel']==0){
+    $selector = "`sessionToken`='$sessionToken'";
+    $result = crud_read('freeOrdersTimer','',$selector);
+    $data['permissionKey'] = $result[0]['permissionKey'];
+}
+
+$result = (object) [
+    'success' => true,
+    'data' => $data,
+];
+
+echo json_encode($result);
+
+?>
